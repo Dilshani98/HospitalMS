fbf8e8c7276c90d6ae5b1fd5a046a4e511cbf336
nisuri_m <nisurimunasinghe2020@gmail.com>
2021-11-27 20:48:49 +0530

    doctors CRUD operations

--- src/Components/DoctorComponent/NewDoctor.js
+++ src/Components/DoctorComponent/NewDoctor.js
@@ -1,36 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
+import { db } from "../Config/Config"
 
 const NewDoctor = () => {
+    const doctorCollectionRef = collection(db, "doctors");
+
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [address, setAddress] = useState("");
+    const [qualifications, setQualifications] = useState("");
+    const [phoneNo, setPhoneNo] = useState("");
+    const [submitted, setsubmitted] = useState(false);
+
+
+    const createDoctor = async (e) => {
+        e.preventDefault();
+        function refreshPage() {
+            window.location.reload(false);
+        }
+
+        await addDoc(doctorCollectionRef, {
+            firstName: fname,
+            lastName: lname,
+            address: address,
+            qualifications: qualifications,
+            phoneNo: phoneNo
+        });
+        setsubmitted(true);
+
+    };
+
     return (
         <div className="container">
             <h1 className="display-5 text-center">Add a New Doctor</h1>
-            <hr />
             <form>
-                <div class="form-group">
+                <div class="form-group pb-4">
                     <label >First Name</label>
-                    <input type="text" class="form-control" id="exampleFirstName" placeholder="Enter First Name" />
+                    <input type="text" class="form-control" id="exampleFirstName" placeholder="Enter First Name" onChange={(e) => {
+                        setFname(e.target.value);
+                    }} />
 
                 </div>
-                <div class="form-group">
+
+                <div class="form-group pb-4">
                     <label>Last Name</label>
-                    <input type="text" class="form-control" id="exampleLatName" placeholder="Enter Last Name" />
+                    <input type="text" class="form-control" id="exampleLatName" placeholder="Enter Last Name" onChange={(e) => {
+                        setLname(e.target.value);
+                    }} />
 
                 </div>
-                <div class="form-group">
+                <div class="form-group pb-4">
                     <label>Phone Number</label>
-                    <input type="text" class="form-control" id="examplePhone" placeholder="Enter Phone Number" />
+                    <input type="text" class="form-control" id="examplePhone" placeholder="Enter Phone Number" onChange={(e) => {
+                        setPhoneNo(e.target.value);
+                    }} />
 
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <div class="form-group pb-4">
+                    <label>Address</label>
+                    <input type="text" class="form-control" id="exampleAddress" placeholder="Enter Address" onChange={(e) => {
+                        setAddress(e.target.value);
+                    }} />
+
                 </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="form-group pb-4">
+                    <label>Qualifications</label>
+                    <input type="text" class="form-control" id="exampleQualification" placeholder="Enter Qualifications" onChange={(e) => {
+                        setQualifications(e.target.value);
+                    }} />
+
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+
+                <button type="submit" class="btn btn-primary" onClick={createDoctor}>Submit</button>
             </form>
+            <br />
+            {(submitted) && <div class="alert alert-primary" role="alert">
+                The details of the doctor were added successfully!.
+            </div>}
         </div>
     )
 }
