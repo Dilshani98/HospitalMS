429a8d0a52fa6bf07f46f43fd8515022e19d696c
rajitharul <62076329+rajitharul@users.noreply.github.com>
2021-11-28 20:17:13 +0530

    Update FindReport.js

--- src/Components/ReportComponent/FindReport.js
+++ src/Components/ReportComponent/FindReport.js
@@ -1,48 +1,48 @@
 import React, { useState, useEffect } from 'react'
-import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc, setDoc } from 'firebase/firestore';
-import { db } from "../Config/Config"
+import { doc, collection, getDocs, onSnapshot, deleteDoc, updateDoc, getFirestore, } from 'firebase/firestore';
+import { forwardRef } from "react";
+import {
+    FaCheck,
+    FaFileExport,
+    FaPlus,
+    FaRegTrashAlt,
+    FaSearch,
+    FaTimes,
+} from "react-icons/fa";
+import {
+    BiChevronRight,
+    BiChevronsLeft,
+    BiEdit,
+    BiSortAlt2,
+} from "react-icons/bi";
+
+
+import MaterialTable from "material-table";
 import { NavLink } from 'react-router-dom'
 
 const FindReport = () => {
 
-    const reportCollectionRef = collection(db, "reports");
-
     const [reports, setreports] = useState([]);
 
-    const [reportId, setreportId] = useState("");
-    const [submitted, setsubmitted] = useState(false);
-    const [refresh, setRefresh] = useState(false);
-
-    const FindAReport = async (e) => {
-        e.preventDefault();
-        function refreshPage() {
-            window.location.reload(false);
-        }
-
-        await addDoc(reportCollectionRef, {
-            reportId: reportId,
-        });
-        setsubmitted(true);
-        setreportId(reportId);
-
-    };
-
-
     useEffect(() => {
-        getReport();
+        const data = () => {
+            onSnapshot(collection(getFirestore(), "reports"), (snapshot) => {
+                // realtime update
+                setreports(
+                    snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+                );
+            });
+        };
+        data();
+    }, []);
 
-    }, [refresh]);
 
-    const getReport = async () => {
-        const data = await getDocs(reportCollectionRef);
-        setreports(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    const deleteReport = async (id) => {
-        const roomsDoc = doc(db, "reports", id);
-
-        await deleteDoc(roomsDoc);
-        setRefresh(!refresh);
+    const handleDelete = async (oldData, resolve) => {
+        console.log(oldData);
+        const docRef = doc(getFirestore(), "reports", oldData.id);
+        await deleteDoc(docRef);
+        resolve();
+        console.log("succesfully delete");
     };
 
 
@@ -50,63 +50,115 @@ const FindReport = () => {
         <>
             <div className="container">
                 <h1 className="display-5 text-center">Find Medical Reports</h1>
-                <form>
-
-                    <div class="form-group pb-4">
-                        <label >Enter Report ID </label>
-                        <input type="text" class="form-control" id="exampleReportId" placeholder="Enter Report Id to find the Medical Report" onChange={(e) => {
-                            setreportId(e.target.value);
-                        }} />
-
-                    </div>
-                    <button type="submit" class="btn btn-primary" onClick={FindAReport}>Find</button> {""}
-                    <NavLink to="/reports">
-                        <button type="button" class="btn btn-secondary"> Go Back</button>
-                    </NavLink>
-
-                </form>
                 <br />
                 <hr />
-                <div style={{ marginTop: "50px", width: "1000px" }}>
-                    <table className="table table-striped mt-5">
-                        <thead className="table-dark">
-                            <tr>
-                                <th scope="col">Report ID</th>
-                                <th scope="col">Report Name</th>
-                                <th scope="col">Patient Name</th>
-                                <th scope="col">Doctor Name</th>
-                                <th scope="col">Rate</th>
-                                <th scope="col">Description</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
+                <MaterialTable
+                    icons={{
+                        Filter: forwardRef((props, ref) => <FaSearch />),
+                        Search: forwardRef((props, ref) => <FaSearch />),
+                        SortArrow: forwardRef((props, ref) => <BiSortAlt2 />),
+                        Clear: forwardRef((props, ref) => <FaTimes />),
+                        Export: forwardRef((props, ref) => <FaFileExport />),
+                        FirstPage: forwardRef((props, ref) => (
+                            <h1 style={{ fontSize: "15px" }}>First Page</h1>
+                        )),
+                        LastPage: forwardRef((props, ref) => (
+                            <h1 style={{ fontSize: "15px" }}>Last Page</h1>
+                        )),
+                        NextPage: forwardRef((props, ref) => <BiChevronRight />),
+                        PreviousPage: forwardRef((props, ref) => <BiChevronsLeft />),
+                        ResetSearch: forwardRef((props, ref) => (
+                            <h1 style={{ fontSize: "15px" }}>Clear</h1>
+                        )),
+                        Edit: forwardRef((props, ref) => <BiEdit />),
+                        Check: forwardRef((props, ref) => <FaCheck />),
+                        Delete: forwardRef((props, ref) => <FaRegTrashAlt />),
+                        Add: forwardRef((props, ref) => <FaPlus />),
+                    }}
+                    columns={[
+                        {
+                            title: "Report Id",
+                            field: "reportId",
+                            editable: "never",
+                        },
+                        {
+                            title: "Report Name",
+                            field: "reportName",
+                            render: (rowData) => (
+                                <span className="">
+                                    {rowData.reportName}
+                                    {/* {(new Date(rowData.date*1000).toUTCString())}
+                   {console.log(new Date(rowData.date*1000).toUTCString())} */}
+                                    {/* {console.log(rowData.date)} */}
+                                </span>
+                            ),
+                        },
+                        {
+                            title: "Rate",
+                            field: "rate",
+                            render: (rowData) => (
+                                <span className="">{rowData.rate}</span>
+                            ),
+                        },
+                        {
+                            title: "Doctor's Name",
+                            field: "doctorName",
 
-                        {(submitted) &&
-                            <tbody key={reports.id}>
-                                <tr className="table-primary mt-5 hide-table-padding table-details">
-                                    <td scope="row">{reports.reportId}</td>
-                                    <td>{reports.reportName}</td>
-                                    <td>{reports.patientName}</td>
-                                    <td>{reports.doctorName}</td>
-                                    <td>{reports.rate}</td>
-                                    <td>{reports.description}</td>
-                                    <td>
-                                        <button
-                                            className='col c-s-btn'
-                                            onClick={() => deleteReport(reports.id)}
-                                            style={{ paddingLeft: "0", marginLeft: "0", width: "60px" }}>
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
+                            sorting: false,
 
-                            </tbody>
-
-
-                        }
-
-                    </table>
-                </div>
+                            render: (rowData) => (
+                                <span>
+                                    {rowData.doctorName}
+                                    {console.log(rowData.doctorName)}
+                                </span>
+                            ),
+                        },
+                        {
+                            title: "Patient's Name",
+                            field: "patientName",
+                            searchable: false,
+                            sorting: false,
+                            render: (rowData) => (
+                                <span>
+                                    {rowData.patientName}
+                                </span>
+                            ),
+                        },
+                        {
+                            title: "Description",
+                            field: "description",
+                            searchable: false,
+                            sorting: false,
+                            render: (rowData) => (
+                                <span>
+                                    {rowData.description}
+                                </span>
+                            ),
+                        },
+                        // {
+                        //   title: "Doğum Yeri",
+                        //   field: "birthCity",
+                        //   lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+                        // },
+                    ]}
+                    data={reports}
+                    options={{
+                        sorting: true,
+                        filtering: true,
+                        actionsColumnIndex: -1,
+                        selection: false,
+                    }}
+                    title=""
+                    actions={[]}
+                    editable={{
+                        onRowDelete: (oldData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                    handleDelete(oldData, resolve);
+                                }, 1000);
+                            }),
+                    }}
+                />
 
 
             </div>
